8f1fc33fffd09497836516b2eeff1d764165c01d
jason8ni <jason8ni@hotmail.com>
2018-10-30 23:08:54 -0400

    melody extraction

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,7 +2,7 @@
 
 from __future__ import print_function
 
-from music21 import converter, corpus, instrument, midi, note, chord, pitch
+from music21 import converter, corpus, instrument, midi, note, chord, pitch, stream
 from collections import defaultdict, OrderedDict
 from itertools import groupby, zip_longest
 
@@ -15,7 +15,19 @@ def list_instruments(midi):
 
 def parseMidi(filename):
     #Parse melody and accompaniment separately
-    midiData = converter.parse(filename).show()
+    midiData = converter.parse(filename)
+    melody1, melody2 = midiData[0].getElementsByClass(stream.Voice)
+    print(midiData[1])
+    for j in melody2:
+        print(j)
+        melody1.insert(j.offset, j)
+    melody_voice = melody1
+
+    for i in melody_voice:
+        if i.quarterLength == 0.0:
+            i.quarterLength = 0.25
+    
 
 filePath = "./MusicFiles/bach_minuet.mid"
 parseMidi(filePath)
+
