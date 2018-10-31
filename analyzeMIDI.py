d758dc9aafa8453528816ef72a434db2386a8421
jason8ni <jason8ni@hotmail.com>
2018-10-30 21:11:23 -0400

    Use music21 to parse midi file

---
+++ analyzeMIDI.py
@@ -0,0 +1,10 @@
+def printMIDI(filename):
+    from mido import MidiFile
+    mid = MidiFile(filename)
+    for i, track in enumerate(mid.tracks):
+        print('Track {}: {}'.format(i, track.name))
+        for message in track:
+            print(message)
+
+
+printMIDI('./MusicFiles/bach_minuet.mid')
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,12 +1,21 @@
-#!/usr/bin/python3
 #This class handles the proprocessing of the MIDI files required to start training
 
 from __future__ import print_function
 
-from music21 import *
+from music21 import converter, corpus, instrument, midi, note, chord, pitch
 from collections import defaultdict, OrderedDict
 from itertools import groupby, zip_longest
 
+def list_instruments(midi):
+    partStream = midi.parts.stream()
+    print("List of instruments found on MIDI file:")
+    for p in partStream:
+        aux = p
+        print (p.partName)
+
 def parseMidi(filename):
     #Parse melody and accompaniment separately
-    midiData = converter.parse(filename)
+    midiData = converter.parse(filename).show()
+
+filePath = "./MusicFiles/bach_minuet.mid"
+parseMidi(filePath)
