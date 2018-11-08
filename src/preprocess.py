98f76a310031178d248d415e122e1da2c9ec6f08
jason8ni <jason8ni@hotmail.com>
2018-11-07 21:57:22 -0500

    Edit

--- src/preprocess.py
+++ src/preprocess.py
@@ -6,131 +6,39 @@ from music21 import converter, corpus, instrument, midi, note, chord, pitch, str
 from collections import defaultdict, OrderedDict
 from itertools import groupby, zip_longest
 import sys, os
-import numpy as numpy
+import numpy as np
 import midi
 
 RANGE = 128
 
-def list_instruments(midi):
-    partStream = midi.parts.stream()
-    print("List of instruments found on MIDI file:")
-    for p in partStream:
-        aux = p
-        print (p.partName)
+def round_tick(tick, time_step):
+    return int(round(tick/float(time_step)) * time_step)
 
-def parseMidi(filename):
-    #Parse melody and accompaniment separately
-    midiData = converter.parse(filename)
-    melody1, melody2 = midiData[0].getElementsByClass(stream.Voice)
-    print(midiData[1])
-    for j in melody2:
-        print(j)
-        melody1.insert(j.offset, j)
-    melody_voice = melody1
 
-    for i in melody_voice:
-        if i.quarterLength == 0.0:
-            i.quarterLength = 0.25
-    
-    partIndices = [0, 1, 6, 7]
-    comp_stream = stream.Voice()
-    comp_stream.append([j.flat for i, j in enumerate(midi_data) 
-        if i in partIndices])
-
-    # Full stream containing both the melody and the accompaniment. 
-    # All parts are flattened. 
-    full_stream = stream.Voice()
-    for i in range(len(comp_stream)):
-        full_stream.append(comp_stream[i])
-    full_stream.append(melody_voice)
-
-    # Extract solo stream, assuming you know the positions ..ByOffset(i, j).
-    # Note that for different instruments (with stream.flat), you NEED to use
-    # stream.Part(), not stream.Voice().
-    # Accompanied solo is in range [478, 548)
-    solo_stream = stream.Voice()
-    for part in full_stream:
-        curr_part = stream.Part()
-        curr_part.append(part.getElementsByClass(instrument.Instrument))
-        curr_part.append(part.getElementsByClass(tempo.MetronomeMark))
-        curr_part.append(part.getElementsByClass(key.KeySignature))
-        curr_part.append(part.getElementsByClass(meter.TimeSignature))
-        curr_part.append(part.getElementsByOffset(476, 548, 
-                                                  includeEndBoundary=True))
-        cp = curr_part.flat
-        solo_stream.insert(cp)
-
-    # Group by measure so you can classify. 
-    # Note that measure 0 is for the time signature, metronome, etc. which have
-    # an offset of 0.0.
-    melody_stream = solo_stream[-1]
-    measures = OrderedDict()
-    offsetTuples = [(int(n.offset / 4), n) for n in melody_stream]
-    measureNum = 0 # for now, don't use real m. nums (119, 120)
-    for key_x, group in groupby(offsetTuples, lambda x: x[0]):
-        measures[measureNum] = [n[1] for n in group]
-        measureNum += 1
-
-    # Get the stream of chords.
-    # offsetTuples_chords: group chords by measure number.
-    chordStream = solo_stream[0]
-    chordStream.removeByClass(note.Rest)
-    chordStream.removeByClass(note.Note)
-    offsetTuples_chords = [(int(n.offset / 4), n) for n in chordStream]
-
-    # Generate the chord structure. Use just track 1 (piano) since it is
-    # the only instrument that has chords. 
-    # Group into 4s, just like before. 
-    chords = OrderedDict()
-    measureNum = 0
-    for key_x, group in groupby(offsetTuples_chords, lambda x: x[0]):
-        chords[measureNum] = [n[1] for n in group]
-        measureNum += 1
-
-    # Fix for the below problem.
-    #   1) Find out why len(measures) != len(chords).
-    #   ANSWER: resolves at end but melody ends 1/16 before last measure so doesn't
-    #           actually show up, while the accompaniment's beat 1 right after does.
-    #           Actually on second thought: melody/comp start on Ab, and resolve to
-    #           the same key (Ab) so could actually just cut out last measure to loop.
-    #           Decided: just cut out the last measure. 
-    del chords[len(chords) - 1]
-    assert len(chords) == len(measures)
-
-    return measures, chords
-
-filePath = "./MusicFiles/bach_minuet.mid"
-parseMidi(filePath)
-
-def ingest_notes(track, verbose=False):
+def ingest_notes(track):
 
     notes = { n: [] for n in range(RANGE) }
     current_tick = 0
 
     for msg in track:
-        # ignore all end of track events
         if isinstance(msg, midi.EndOfTrackEvent):
             continue
 
         if msg.tick > 0: 
             current_tick += msg.tick
 
-        # velocity of 0 is equivalent to note off, so treat as such
+        # velocity of 0 is the note off, so treat as such
         if isinstance(msg, midi.NoteOnEvent) and msg.get_velocity() != 0:
             if len(notes[msg.get_pitch()]) > 0 and \
                len(notes[msg.get_pitch()][-1]) != 2:
-                if verbose:
-                    print("Warning: double NoteOn encountered, deleting the first")
-                    print(msg)
+                print("Warning: double NoteOn encountered, deleting the first")
             else:
                 notes[msg.get_pitch()] += [[current_tick]]
         elif isinstance(msg, midi.NoteOffEvent) or \
             (isinstance(msg, midi.NoteOnEvent) and msg.get_velocity() == 0):
             # sanity check: no notes end without being started
             if len(notes[msg.get_pitch()][-1]) != 1:
-                if verbose:
-                    print("Warning: skipping NoteOff Event with no corresponding NoteOn")
-                    print(msg)
+                print("Warning: skipping NoteOff Event with no corresponding NoteOn")
             else: 
                 notes[msg.get_pitch()][-1] += [current_tick]
 
