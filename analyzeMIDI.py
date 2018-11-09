71bd1207a2e575fdb6697b7030fa68206cf77199
jason8ni <jason8ni@hotmail.com>
2018-11-08 21:11:08 -0500

    print pattern

--- analyzeMIDI.py
+++ analyzeMIDI.py
@@ -1,10 +1,10 @@
+from mido import MidiFile
+
+import midi
+
 def printMIDI(filename):
-    from mido import MidiFile
-    mid = MidiFile(filename)
-    for i, track in enumerate(mid.tracks):
-        print('Track {}: {}'.format(i, track.name))
-        for message in track:
-            print(message)
 
+    mid = midi.read_midifile(filename)
+    print(mid)
 
-printMIDI('./MusicFiles/bach_minuet.mid')
+printMIDI('./MusicFiles/Unravel.mid')
