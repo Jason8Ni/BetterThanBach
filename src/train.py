da5f77e64f377cab5d92fd193edc375fe28427fe
jason8ni <jason8ni@hotmail.com>
2018-11-12 21:49:23 -0500

    gibbs chain

--- src/train.py
+++ src/train.py
@@ -26,3 +26,26 @@ def menu(songs):
     biases = {
         'out': tf.Variable(tf.random_normal([1, noteRange]))
     }
+
+    X = tf.placeholder(tf.float32, [None, noteRange], name = "X")
+    
+def sample(probability):
+    #Takes in a vector of probabilities, and returns a random vector of 0s and 1s sampled from the input vector
+    return tf.floor(probability + tf.random_uniform(tf.shape(probability), 0, 1))
+
+def gibbs_sample(k):
+        #Runs a k-step gibbs chain to sample from the probability distribution of the RBM defined by W, bh, bv
+        def gibbs_step(count, k, xk):
+            #Runs a single gibbs step. The visible values are initialized to xk
+            hk = sample(tf.sigmoid(tf.matmul(xk, W) + bh)) #Propagate the visible values to sample the hidden values
+            xk = sample(tf.sigmoid(tf.matmul(hk, tf.transpose(W)) + bv)) #Propagate the hidden values to sample the visible values
+            return count+1, k, xk
+
+        #Run gibbs steps for k iterations
+        ct = tf.constant(0) #counter
+        [_, _, x_sample] = control_flow_ops.while_loop(lambda count, num_iter, *args: count < num_iter,
+                                            gibbs_step, [ct, tf.constant(k), x])
+        #This is not strictly necessary in this implementation, but if you want to adapt this code to use one of TensorFlow's
+        #optimizers, you need this in order to stop tensorflow from propagating gradients back through the gibbs step
+        x_sample = tf.stop_gradient(x_sample) 
+        return x_sample
